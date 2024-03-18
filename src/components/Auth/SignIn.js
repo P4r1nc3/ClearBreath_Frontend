d9f4a1af1c64308f6aeebe4078bf581baf3a3744
P4r1nc3 <konrad.tupta@gmail.com>
2024-03-18 20:05:59 +0100

    Adjusted SignIn

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -34,16 +34,12 @@ const SignIn = () => {
                         </h1>
                         <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                             <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
                                 <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@example.com" required />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
                                 <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
-                                <input type="password" name="confirm-password" id="confirm-password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="••••••••" required />
                             </div>
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
@@ -53,7 +49,11 @@ const SignIn = () => {
                                     <label htmlFor="terms" className="font-light text-gray-500">I accept the <a href="#" className="font-medium text-primary-600 hover:underline">Terms and Conditions</a></label>
                                 </div>
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign In</button>
+                            <button type="submit" className="w-full text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                Sign In
+                            </button>
+
+
                             <p className="text-sm font-light text-gray-500">
                                 Don't have an account? <a href="/signup" className="font-medium text-primary-600 hover:underline">Sign Up here</a>
                             </p>
