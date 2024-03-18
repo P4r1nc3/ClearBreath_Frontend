2a281455f81ec58d27ead344077085d91bacc951
P4r1nc3 <konrad.tupta@gmail.com>
2024-03-18 17:16:13 +0100

    Made usage of apiRequest functions

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,39 +1,14 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://localhost:8080';
-
-// Generic API service function
-const apiRequest = async (url, method, data = null) => {
-    const options = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: data,
-    };
-    try {
-        const response = await axios(url, options);
-        return response.data;
-    } catch (error) {
-        if (error.response) {
-            throw new Error(error.response.data.message || 'Something went wrong');
-        } else if (error.request) {
-            throw new Error('Request failed');
-        } else {
-            throw new Error('Something went wrong');
-        }
-    }
-};
+import { BASE_URL } from '../constants/urlConstants';
+import { apiRequestWithoutAuth } from "./apiRequest";
 
 export const signUp = async (firstName, lastName, email, password) => {
     const url = `${BASE_URL}/auth/signup`;
     const data = { firstName, lastName, email, password };
-    return await apiRequest(url, 'POST', data);
+    return await apiRequestWithoutAuth(url, 'POST', data);
 };
 
 export const signIn = async (email, password) => {
     const url = `${BASE_URL}/auth/signin`;
     const data = { email, password };
-    return await apiRequest(url, 'POST', data);
+    return await apiRequestWithoutAuth(url, 'POST', data);
 };
-
--- src/api/marker.js
+++ src/api/marker.js
@@ -1,44 +1,17 @@
-import axios from 'axios';
-import { getTokenFromLocalStorage } from "../helpers/tokenStorage";
-
-const BASE_URL = 'http://localhost:8080';
-
-// Generic API service function
-const apiRequest = async (url, method, data = null) => {
-    const token = getTokenFromLocalStorage();
-    const options = {
-        method: method,
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        },
-        data: data,
-    };
-    try {
-        const response = await axios(url, options);
-        return response.data;
-    } catch (error) {
-        if (error.response) {
-            throw new Error(error.response.data.message || 'Something went wrong');
-        } else if (error.request) {
-            throw new Error('Request failed');
-        } else {
-            throw new Error('Something went wrong');
-        }
-    }
-};
+import { BASE_URL } from '../constants/urlConstants';
+import { apiRequestWithAuth } from "./apiRequest";
 
 export const saveMarker = async (lat, lng) => {
     const url = `${BASE_URL}/markers/lat/${lat}/lng/${lng}`;
-    return await apiRequest(url, 'POST');
+    return await apiRequestWithAuth(url, 'POST');
 };
 
 export const getMarkers = async () => {
     const url = `${BASE_URL}/markers`;
-    return await apiRequest(url, 'GET');
+    return await apiRequestWithAuth(url, 'GET');
 };
 
 export const deleteMarker = async (lat, lng) => {
     const url = `${BASE_URL}/markers/lat/${lat}/lng/${lng}`;
-    await apiRequest(url, 'DELETE');
+    await apiRequestWithAuth(url, 'DELETE');
 };
