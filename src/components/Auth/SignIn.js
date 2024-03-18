5cddfc96deccb8ef1efe788d96d1bf977da15597
P4r1nc3 <konrad.tupta@gmail.com>
2024-03-18 20:02:01 +0100

    Remade SignIn

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -7,6 +7,7 @@ import PasswordInput from "../PasswordInput";
 const SignIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const navigate = useNavigate();
     const { login } = useAuth();
@@ -24,32 +25,43 @@ const SignIn = () => {
     };
 
     return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="card">
-                        <div className="card-header">
-                            <h3 className="text-center">Sign In</h3>
-                        </div>
-                        <div className="card-body">
-                            <form onSubmit={handleSubmit}>
-                                <div className="form-group mb-3">
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" required />
-                                </div>
+        <section>
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Sign In
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@example.com" required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
                                 <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
-                                <div className="text-center mb-3">
-                                    <button type="submit" className="btn btn-primary">Sign In</button>
+                            </div>
+                            <div>
+                                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                                <input type="password" name="confirm-password" id="confirm-password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="••••••••" required />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required />
                                 </div>
-                            </form>
-                        </div>
-                        <div className="card-footer text-center">
-                            <p>Don't have an account? <a href="/src/components/Auth/SignUp" className="link">Sign Up</a></p>
-                        </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="terms" className="font-light text-gray-500">I accept the <a href="#" className="font-medium text-primary-600 hover:underline">Terms and Conditions</a></label>
+                                </div>
+                            </div>
+                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign In</button>
+                            <p className="text-sm font-light text-gray-500">
+                                Don't have an account? <a href="/signup" className="font-medium text-primary-600 hover:underline">Sign Up here</a>
+                            </p>
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
